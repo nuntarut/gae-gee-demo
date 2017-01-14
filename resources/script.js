42e3e2f627fdcd4b564571877b82295940dd1d76
nuntarut@outlook.com <nuntarut@outlook.com>
2017-01-14 18:37:45 +0700

    Adding an incomplete JS

--- resources/script.js
+++ resources/script.js
@@ -38,6 +38,40 @@ function initMap() {
       }  	  
 	
 	
+	window.eqfeed_callback = function(results) {
+			for (var i = 0; i < results.features.length; i++) {
+				var coords = results.features[i].geometry.coordinates;
+				var latLng = new google.maps.LatLng(coords[1].coords[0]);
+				var marker = new google.maps.marker({
+						position: latLng,
+						
+				})
+			}
+	}
+	
+
+	function showEEMap(eeMapId, eToken) {
+			console.log("EE Map ID: " + eeToken);
+			console.log("EE Token: " + eeToken);
+			
+			eeMapType = new google.maps.ImageMapType({
+				'name': 'ecomap',
+				'opacity': 1.0,
+				'tileSize': new google.maps.Size(256,256),
+				'getTileUrl': function(tile,zoom) {
+						return 'https://earthengine.googleapis.com/map/'
+							+ eeMapID + '/' + zoom + '/' + tile.x + '/' + tile.y
+							+ '?token=' + eeToken;
+				}
+			});
+			
+			map.overlayMapTypes.push(eeMapType);
+	}
+							
+							
+				}
+			})
+	}
 
 
 
